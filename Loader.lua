5f9967bf90b83d940295765f69c1e721233273ee
doanhungff123123-commits <doanhungff123123@gmail.com>
2026-02-05 11:11:49 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,7 @@
+if getgenv().LuckyBloxLoaded then return end
+getgenv().LuckyBloxLoaded = true
+
+loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/doanhungff123123-commits/luckybloxmoney/main/Main.lua",
+    true
+))()
